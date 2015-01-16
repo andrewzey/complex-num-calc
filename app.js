14d6ffd4a11dc02166c470ff2d4b0b5c359a740d
Andrew Zey <andrew@andrewzey.com>
2015-01-15 18:09:16 -0800

    make clear button work

--- app.js
+++ app.js
@@ -3,12 +3,26 @@
 $(function(){
   'use strict';
 
+  var $display = $('input');
+  var resReal = 5;
+  var resImaginary = 10;
+  var display = resReal + ' + ' + resImaginary + 'i';
+  console.log(display);
+
+  var clearTotal = function(val){
+    resReal = 0;
+    resImaginary = 0;
+    $('input').focus().val('');
+  };
+
   var performOperation = function(operation) {
     if (operation === '+') { console.log('clicked add'); }
     if (operation === '-') { console.log('clicked subtract'); }
     if (operation === '*') { console.log('clicked multiply'); }
     if (operation === '/') { console.log('clicked divide'); }
-    if (operation === 'c') { console.log('clicked clear'); }
+    if (operation === 'c') {
+      clearTotal();
+    }
     if (operation === '=') { console.log('clicked equals'); }
   };
 
@@ -17,5 +31,5 @@ $(function(){
     var operation = $(this).text();
     performOperation(operation);
   });
-  
+
 });
