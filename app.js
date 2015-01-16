a8ef6d2d771043951682d71185772d10d2aafe84
Andrew Zey <andrew@andrewzey.com>
2015-01-15 18:00:17 -0800

    setup click handlers on buttons

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+/*global $:false */
 
+$(function(){
+  'use strict';
+
+  var performOperation = function(operation) {
+    if (operation === '+') { console.log('clicked add'); }
+    if (operation === '-') { console.log('clicked subtract'); }
+    if (operation === '*') { console.log('clicked multiply'); }
+    if (operation === '/') { console.log('clicked divide'); }
+    if (operation === 'c') { console.log('clicked clear'); }
+    if (operation === '=') { console.log('clicked equals'); }
+  };
+
+  //click handlers for buttons
+  $('.buttonsRow').on('click', 'button', function(){
+    var operation = $(this).text();
+    performOperation(operation);
+  });
+  
+});
