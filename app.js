165d4537f119035e9c7fce99357f885230ca6616
Andrew Zey <andrew@andrewzey.com>
2015-01-15 18:23:43 -0800

    make add button work

--- app.js
+++ app.js
@@ -3,20 +3,32 @@
 $(function(){
   'use strict';
 
-  var $display = $('input');
-  var resReal = 5;
-  var resImaginary = 10;
-  var display = resReal + ' + ' + resImaginary + 'i';
-  console.log(display);
+  var resReal = 0;
+  var resImag = 0;
+
+  var formatDisplayTotal = function(real, imaginary) {
+    return real + ' + ' + imaginary + 'i';
+  };
+
+  var updateDisplay = function(displayString){
+    $('input').focus().val(displayString);
+  };
 
   var clearTotal = function(val){
     resReal = 0;
-    resImaginary = 0;
+    resImag = 0;
     $('input').focus().val('');
   };
 
   var performOperation = function(operation) {
-    if (operation === '+') { console.log('clicked add'); }
+    if (operation === '+') {
+      var userInput = $('input').val().split(' + ');
+      var userInputReal = parseInt(userInput[0]);
+      var userInputImag = parseInt(userInput[1]);
+      resReal = resReal + userInputReal;
+      resImag = resImag + userInputImag;
+      updateDisplay(formatDisplayTotal(resReal, resImag));
+    }
     if (operation === '-') { console.log('clicked subtract'); }
     if (operation === '*') { console.log('clicked multiply'); }
     if (operation === '/') { console.log('clicked divide'); }
